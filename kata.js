f3736d85fa5d06d5a8c2cd1535cf3c8f570a5b20
Lukas Gisder-Dubé <gisderdube@gmail.com>
2019-03-21 12:56:23 +0100

    stuff

---
+++ kata.js
@@ -0,0 +1,30 @@
+// Goal: Extract from a two dimensional array (with the same height and width), concat all the elements from the "second diagonal", starting from the top right corner to the bottom left corner
+// function concatSecondDiagonal(twoDimArray) {
+//     let finalString = ''
+
+//     for (let index = 0; index < twoDimArray.length; index++) {
+//         const nestedArr = twoDimArray[index]
+//         const char = nestedArr[nestedArr.length - 1 - index]
+//         finalString += char
+//     }
+//     return finalString
+// }
+
+function concatSecondDiagonal(twoDimArray) {
+    const charArray = twoDimArray.map((el, index, array) => {
+        return el[array.length - 1 - index]
+    })
+    return charArray.join('')
+}
+
+console.log(
+    concatSecondDiagonal([
+        ['a', 'z', 'x', 'C', 'v', 'B'],
+        ['t', 'w', 'I', 'p', 'e', '7'],
+        ['w', 'y', 'o', 'r', 'm', 'x'],
+        ['q', 'U', 'l', 's', 'k', 'H'],
+        ['A', 'i', 'G', 'Y', 'h', 'L'],
+        ['n', 'f', 'd', 'W', 'z', 'a'],
+    ])
+)
+// ==> "Berlin"
